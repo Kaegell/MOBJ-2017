8419192c933e46c75a88558e51019eca3cd07f2a
Kaegell <kev.mambu@gmail.com>
2017-10-12 10:57:24 +0200

    backInsert behaves as intended (this piece of sh#t... -_-)

--- TME3/src/Main.cpp
+++ TME3/src/Main.cpp
@@ -1,10 +1,46 @@
-using namespace vector_bench;
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <algorithm>
+#include "Timer.h"
+#include "GPL_2_text.h"
+
+using namespace std;
+
+void backInsert()
+{
+	vector<string> GPL_2_vector (GPL_2_text, GPL_2_text + sizeof(GPL_2_text)/sizeof(char*));
+	cerr << "GPL_2_vector created" << endl;
+	
+	vector<string>::iterator it;
+	vector<string>::iterator it_begin = GPL_2_vector.begin();
+	vector<string>::iterator it_end = GPL_2_vector.end();
+
+	for(vector<string>::const_iterator it = it_begin; it != it_end; ++it)
+	{
+		cout << *it << " ";
+	}
+
+	sort(GPL_2_vector.begin(),GPL_2_vector.end());
+
+
+	for(vector<string>::const_iterator it = it_begin; it != it_end; ++it)
+	{
+		cout << *it << " ";
+	}
+	cout << endl;
+}
 
 int main ( int argc, char* argv[] )
 {
-  backInsert    ();
-  frontInsert   ();
-  sortEachInsert();
 
-  return 0;
+	Timer t;
+	t = t.start();
+	backInsert    ();
+	t = t.stop();
+	cout << "backInsert exec. time : " << t << endl;
+	//frontInsert   ();
+	//sortEachInsert();
+
+	return 0;
 }
